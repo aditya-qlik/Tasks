b9af764dba6d1a0a37c634536ebe1ebcd72fbf07
AdityA <aditya@Aditya-PC.local>
2020-02-25 10:27:33 +0530

    Part-B attempt-1

--- Week-2/Array_task.js
+++ Week-2/Array_task.js
@@ -1,5 +1,6 @@
 const logArrayButton = document.querySelector('#logArray');
 const logArrayContentButton = document.querySelector('#arrayContent');
+const arrayCheckButton = document.querySelector('#arrayCheck');
 const numbers = Array.from(
         {
             length: 101,
@@ -12,5 +13,28 @@ function logArray() {
 function logArrayContents() {
         numbers.forEach(content => console.log(content));
 }
+function checkDivisibilityBy3and5(number){
+        return number % 3 === 0 && number % 5 === 0;
+}
+function checkDivisibilityBy3(number) {
+        return number % 3 === 0;
+}
+function checkDivisibilityBy5(number) {
+        return number % 5 === 0;
+}
+function checkArray(arrayToCheck){
+        for (number of arrayToCheck) {
+                if(checkDivisibilityBy3and5(number)){
+                        console.log('Fizz Buzz');
+                } else if(checkDivisibilityBy5(number)){
+                        console.log('Buzz');
+                } else if(checkDivisibilityBy3(number)){
+                        console.log('Fizz');
+                } else{
+                        console.log(number);
+                }
+        }
+}
 logArrayButton.addEventListener('click', logArray);
 logArrayContentButton.addEventListener('click', logArrayContents);
+arrayCheckButton.addEventListener('click',() => checkArray(numbers));
