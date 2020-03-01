8140fe5b3b8398789951474703b18507f9297670
aditya-qlik <aditya.kumar@qliktag.com>
2020-03-01 13:28:05 +0530

    async_await_task

---
+++ Task_On_APIs/async_await_task/async_awaita-task.js
@@ -0,0 +1,27 @@
+const baseEndpoint = `https://api.github.com`;
+const userEndpoint = `${baseEndpoint}/users`;
+const userArray = [];
+
+async function userInfoArray(userInfo) {
+    const userData = await userInfo;
+    userArray.push(userData);
+    console.table(userArray);
+}
+
+async function fetchUserData(){
+    const username = document.querySelector('#name').value;
+    const response = await fetch(`${userEndpoint}/${username}`).catch(handleError);
+    if (response.status !== 200){
+        handleError(response.statusText);
+    } else {
+        const data = await response.json();
+        console.log(data);
+        userInfoArray(data);
+    }
+}
+function handleError(err) {
+    console.log("Theres no user with that username")
+    console.log(err);
+    return;
+}
+document.querySelector('#submit').addEventListener('click',fetchUserData);
