8a729ad1090d0ea6a68bad38bfb33da31b2a6177
Aditya Kumar <60967871+aditya-qlik@users.noreply.github.com>
2020-03-04 11:28:15 +0530

    Update division.js

--- bundle_task/division.js
+++ bundle_task/division.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 function division(valueOfNum1 = 0, valueOfNum2 = 1) {
-        console.log(valueOfNum2)
         if (valueOfNum2 === 0) {
                 console.error(`2nd Number can't be 0`);
                 return `<strong style="color:red;">ERROR!!!! 2nd Number can't be 0</strong>`;
